545ed4455909aea310f3fcc2192d5ea8b1b76302
Felix Prava <mihai_prava@yahoo.com>
2020-05-16 20:27:11 +0300

    Primul buton

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,20 +4,31 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 375));
+        primaryStage.setTitle("Dentisti-Tg-Jiu");
+
+        Button button = new Button();
+        button.setText("Sign Up");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().addAll(button);
+
+        Scene scene = new Scene(layout, 400, 375);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
