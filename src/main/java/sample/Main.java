04b1afe7c3b5e60c0908a31427dcb71ea37fcae7
Felix Prava <mihai_prava@yahoo.com>
2020-05-20 18:30:51 +0300

    FP-1/adaugat DB.json

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,5 +1,7 @@
 package sample;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -11,6 +13,14 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Iterator;
 
 public class Main extends Application {
 
@@ -55,6 +65,8 @@ public class Main extends Application {
         Label l3 = new Label("        Ai deja cont?");
         GridPane.setConstraints(l3, 2, 1);
 
+        Persoana user = new Persoana();
+
         TextField text1 = new TextField();
         text1.setPromptText("Username");
         GridPane.setConstraints(text1, 2, 2);
@@ -64,6 +76,13 @@ public class Main extends Application {
         GridPane.setConstraints(text2, 2, 3);
 
         Button signIn = new Button("Sign In");
+        signIn.setOnAction(e ->
+        {
+            user.setUser(text1.getText());
+            user.setParola(encryptPassword(text2.getText(), user.getUser()));
+            System.out.print(user.getUser() + user.getParola());
+            loginUser(user);
+        });
         GridPane.setConstraints(signIn, 2, 4);
 
         Button forgot = new Button("Ai uitat parola?");
@@ -90,6 +109,7 @@ public class Main extends Application {
 
         //Buton Gata! + mesaj corespunzator
 
+
         parola_Uitata.setOnAction(e ->
                 {
                     if (!usernameParola.getText().equals("")) {
@@ -105,5 +125,52 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+    private void loginUser(Persoana user) {
+        try {
+            byte[] jsonData = Files.readAllBytes(Paths.get("DB2.json"));
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode rootNode = objectMapper.readTree(jsonData);
+            JsonNode userNode = rootNode.path("nume_de_utilizator");
 
+
+            Iterator<JsonNode> elements = userNode.elements();
+            while (elements.hasNext()) {
+                JsonNode username = elements.next();
+                System.out.println(username.asText());
+                if (username.asText().equals(user.getUser())) {
+                    JsonNode parola = username.findParent("nume_de_utilizator").path("parola");
+                    if (parola.asText().equals(user.getParola()))
+                        System.out.println("E ok");
+                    else
+                        System.out.println("Parola gresita");
+                }
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private String encryptPassword(String text, String salt) {
+        MessageDigest md = getMessageDigest();
+        md.update(salt.getBytes(StandardCharsets.UTF_8));
+
+        byte[] hashedPassword = md.digest(text.getBytes(StandardCharsets.UTF_8));
+
+        return new String(hashedPassword, StandardCharsets.UTF_8)
+                .replace("\"", "");
+
+    }
+
+
+    private MessageDigest getMessageDigest() {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("SHA-256 does not exist!");
+        }
+        return md;
+    }
 }
