8fc6ddf1245fb4d99f8aef7e9ce208568254b200
Felix Prava <mihai_prava@yahoo.com>
2020-06-13 20:56:15 +0300

    FP-11/ObservableList<Doctor> complete

--- src/main/java/sample/Scenes/ListaDoctori.java
+++ src/main/java/sample/Scenes/ListaDoctori.java
@@ -1,46 +1,53 @@
 package sample.Scenes;
 
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.json.JSONArray;
+import sample.Users.Doctor;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ListaDoctori {
 
     public static Scene inregis(Stage primaryStage, Scene scene) {
 
-        GridPane grid2 = new GridPane();
-        grid2.setPadding(new Insets(10, 10, 10, 10));
-        grid2.setVgap(8);
-        grid2.setHgap(10);
-        Scene sceneInregistrare = new Scene(grid2, 400, 250);
-        javafx.scene.control.Label l24 = new javafx.scene.control.Label("Test:");
-        GridPane.setConstraints(l24, 0, 0);
-        javafx.scene.control.Label l24a = new javafx.scene.control.Label("Test2:");
-        GridPane.setConstraints(l24a, 0, 1);
-        javafx.scene.control.Label l24b = new javafx.scene.control.Label("Test3:");
-        GridPane.setConstraints(l24b, 0, 2);
-        javafx.scene.control.Label l24c = new javafx.scene.control.Label("Test4:");
-        GridPane.setConstraints(l24c, 0, 3);
-        javafx.scene.control.TextField text1 = new javafx.scene.control.TextField();
-        GridPane.setConstraints(text1, 1, 0);
-        javafx.scene.control.TextField text2 = new javafx.scene.control.TextField();
-        GridPane.setConstraints(text2, 1, 1);
-        javafx.scene.control.TextField text3 = new javafx.scene.control.TextField();
-        GridPane.setConstraints(text3, 1, 2);
-        javafx.scene.control.TextField text4 = new javafx.scene.control.TextField();
-        GridPane.setConstraints(text4, 1, 3);
-        javafx.scene.control.Button ok = new Button("Gata!");
-        ok.setOnAction(e ->
-        {
-            AlertBox.display("Cont nou24", "Înregistrarea a fost realizată cu succes!!!");
-            primaryStage.setScene(scene);
-        });
-        GridPane.setConstraints(ok, 1, 4);
-        grid2.getChildren().addAll(l24, l24a, l24b, l24c, text1, text2, text3, text4, ok);
-        grid2.setAlignment(Pos.CENTER);
-        return sceneInregistrare;
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll();
+
+        Scene scene2 = new Scene(vBox, 300, 400);
+
+        return scene2;
     }
+
+    public ObservableList<Doctor> getDoctor() {
+        ObservableList<Doctor> doctori = FXCollections.observableArrayList();
+        String first = "src\\main\\resources\\DB.json";
+        String contents = null;
+        try {
+            contents = new String((Files.readAllBytes(Paths.get(first))));
+
+            JSONArray Lista = new JSONArray(contents);
+            for (int i = 0; i < Lista.length(); i++) {
+                if (Lista.getJSONObject(i).getString("rol").equals("Doctor")) {
+                    String a = Lista.getJSONObject(i).getString("nume_de_utilizator");
+                    String b = Lista.getJSONObject(i).getString("email");
+                    String c = Lista.getJSONObject(i).getString("adresa");
+                    String d = Lista.getJSONObject(i).getString("profil");
+                    doctori.add(new Doctor(a, b, c, d));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        doctori.add(new Doctor("a", "b", "c", "d"));
+        return doctori;
+    }
+
+
 }
--- src/main/java/sample/Users/Doctor.java
+++ src/main/java/sample/Users/Doctor.java
@@ -2,7 +2,54 @@ package sample.Users;
 
 public class Doctor {
     private String nume;
+    private String email;
     private String adresa;
     private String profil;
+
+    public Doctor() {
+        this.nume = "";
+        this.email = "";
+        this.adresa = "";
+        this.profil = "";
+    }
+
+    public Doctor(String a, String b, String c, String d) {
+        this.nume = a;
+        this.email = b;
+        this.adresa = c;
+        this.profil = d;
+    }
+
+    public String getProfil() {
+        return profil;
+    }
+
+    public void setProfil(String profil) {
+        this.profil = profil;
+    }
+
+    public String getAdresa() {
+        return adresa;
+    }
+
+    public void setAdresa(String adresa) {
+        this.adresa = adresa;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
 
